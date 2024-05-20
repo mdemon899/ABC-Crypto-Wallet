40d4734539f09342674d30c53f53f3cdf4177224
mdemon899 <mdnorealomemon@gmail.com>
2024-05-20 15:25:00 +0600

    Project Done

--- JS/app.js
+++ JS/app.js
@@ -6,3 +6,29 @@ function openMenu() {
 function closeMenu() {
   navMenu.style.right = "-200px";
 }
+
+window.transitionToPage = function (href) {
+  document.querySelector("body").style.opacity = 0;
+  setTimeout(function () {
+    window.location.href = href;
+  }, 500);
+};
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  document.querySelector("body").style.opacity = 1;
+
+  // Select the login and signup buttons
+  const loginButton = document.querySelector("#loginBtn");
+  const signUpButton = document.querySelector("#signUpBtn");
+
+  // Add event listeners to the login and signup buttons
+  loginButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    window.transitionToPage(this.href);
+  });
+
+  signUpButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    window.transitionToPage(this.href);
+  });
+});
